c11a17536026ffd7e9fe750e441547202a6ea064
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-08-08 16:24:38 -0400

    updated changes

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -6,10 +6,18 @@ public final class FrameworkConstants {
         
     }
 
-    private static final String CHROMEDRIVERPATH = System.getProperty("user.dir")+ "/src/test/resources/executables/chromedriver";
-
-    public static String getChromedriverpath() {
+    private static final String RESOURCESPATH = System.getProperty("user.dir")+ "/src/test/resources" ;
+    private static final String CHROMEDRIVERPATH = RESOURCESPATH + "/executables/chromedriver";
+    private static final String CONFIGFILEPATH = RESOURCESPATH + "/config/config.properties"; 
+    
+    public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
     }
+
+    public static String getConfigFilePath() {
+        return CONFIGFILEPATH;
+    }
+
+    
     
 }
