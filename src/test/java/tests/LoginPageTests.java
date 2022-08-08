0cdd30e3fd135690bd0a4dd42f43d8a6a883c74b
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-08-08 16:25:23 -0400

    updated changes

--- src/test/java/tests/LoginPageTests.java
+++ src/test/java/tests/LoginPageTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
@@ -9,16 +11,25 @@ import driver.DriverManager;
 
 public final class LoginPageTests extends BaseTest {
 
-    protected LoginPageTests() {
+    private LoginPageTests() {
 
     }
 
     @Test
-    public void Test() {
+    public void Test() throws Exception {
 
         Driver.initDriver();
         DriverManager.getDriver().findElement(By.name("q")).sendKeys("Selenium Automation", Keys.ENTER);
 
     }
+
+    @Test
+    public void Test2() throws Exception {
+
+        Driver.initDriver();
+        DriverManager.getDriver().findElement(By.name("q")).sendKeys("Selenium Automation", Keys.ENTER);
+
+    }
+
 
 }
