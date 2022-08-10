babe4fb7b588c6adaff6314bb3c8a194e58fc1bc
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-08-10 16:16:32 -0400

    Login Page

---
+++ src/main/java/pages/OrangeHRMLoginPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import enums.WaitStrategy;
+
+public final class OrangeHRMLoginPage extends BasePage{
+
+    private final By textboxUsername = By.id("txtUsername");
+    private final By textboxPassword = By.xpath("//input[@id = 'txtPassword' and @type='password']");
+    private final By buttonLogin = By.id("btnLogin");
+
+    public OrangeHRMLoginPage enterUserName(String username) {
+
+        sendKeys(textboxUsername, username, WaitStrategy.PRESENCE);
+        return this;
+
+    }
+
+    public OrangeHRMLoginPage enterPassword(String password) {
+        sendKeys(textboxPassword, password, WaitStrategy.PRESENCE);
+        return this;
+    }
+
+    public OrangeHRMHomePage clickLogin() {
+        click(buttonLogin, WaitStrategy.CLICKABLE);
+        return new OrangeHRMHomePage();
+
+    }
+
+    public String getTitle() {
+
+        return getPageTitle();
+        
+    }
+
+
+}
