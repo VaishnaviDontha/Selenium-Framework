cda3e2c0f1635c51297d8388ced0e0c2cded65eb
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-08-10 16:12:25 -0400

    Adding constants into one

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -9,13 +9,18 @@ public final class FrameworkConstants {
     private static final String RESOURCESPATH = System.getProperty("user.dir")+ "/src/test/resources" ;
     private static final String CHROMEDRIVERPATH = RESOURCESPATH + "/executables/chromedriver";
     private static final String CONFIGFILEPATH = RESOURCESPATH + "/config/config.properties"; 
-    
+    private static final int EXPLICITWAIT = 10; 
+
     public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
     }
 
     public static String getConfigFilePath() {
         return CONFIGFILEPATH;
+    }
+
+    public static int getExplicitwait() {
+        return EXPLICITWAIT;
     }
 
     
