7a70ecbb6b760cead9d24bc12cec10e37635fc3c
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-19 15:09:26 -0400

    Added DataProvider

--- src/test/java/tests/OrangeHRMTests.java
+++ src/test/java/tests/OrangeHRMTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pages.OrangeHRMLoginPage;
@@ -11,15 +12,35 @@ public final class OrangeHRMTests extends BaseTest {
 
     }
 
-    @Test
-    public void loginLogoutTests() {
+    @Test(dataProvider = "loginTestData")
+    public void loginLogoutTests(String username, String password) {
 
         OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
         
-        String title = ohlp.enterUserName("Admin").enterPassword("admin123").clickLogin().clickWelcome().clickLogout().getTitle();
+        String title = ohlp.enterUserName(username)
+                            .enterPassword(password)
+                            .clickLogin()
+                            .clickWelcome()
+                            .clickLogout()
+                            .getTitle();
 
         Assertions.assertThat(title).isEqualTo("OrangeHRM");
 
+    }
+
+
+    @DataProvider(parallel = true)
+    public Object[][] loginTestData() {
+
+
+        return new Object[][]{
+            {"Admin","admin123"},
+            {"Admin12","admin"},
+            {"Admin","admin123"},
+            {"Admin","admin123"}
+
+        };
+        
     }
 
 }
