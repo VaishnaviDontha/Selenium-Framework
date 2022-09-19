8c45622db017b1dafd5bb06221df6b9fe60e9d90
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-19 15:07:50 -0400

    Added Constants

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -8,12 +8,21 @@ public final class FrameworkConstants {
 
     private static final String RESOURCESPATH = System.getProperty("user.dir")+ "/src/test/resources" ;
     private static final String CHROMEDRIVERPATH = RESOURCESPATH + "/executables/chromedriver";
+    private static final String FIREFOXDRIVERPATH = RESOURCESPATH + "/executables/geckodriver";
     private static final String CONFIGFILEPATH = RESOURCESPATH + "/config/config.properties"; 
     private static final int EXPLICITWAIT = 10; 
 
     public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
     }
+
+    
+    public static String getFirefoxdriverpath() {
+        System.out.println(FIREFOXDRIVERPATH);
+        return FIREFOXDRIVERPATH;
+    }
+
+
 
     public static String getConfigFilePath() {
         return CONFIGFILEPATH;
