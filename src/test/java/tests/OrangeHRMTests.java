0e36c20c97e1a42f67c073e7eb032983914c1ad5
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-19 15:45:54 -0400

    Added Testdata via Excel

--- src/test/java/tests/OrangeHRMTests.java
+++ src/test/java/tests/OrangeHRMTests.java
@@ -1,5 +1,11 @@
 package tests;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -40,6 +46,56 @@ public final class OrangeHRMTests extends BaseTest {
             {"Admin","admin123"}
 
         };
+        
+    }
+
+    @Test(dataProvider = "loginTestDataViaExcel")
+    public void loginTestDataViaExcelTest(String uname, String pwd) {
+
+        OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
+        
+        String title = ohlp.enterUserName(uname)
+                            .enterPassword(pwd)
+                            .clickLogin()
+                            .clickWelcome()
+                            .clickLogout()
+                            .getTitle();
+
+        Assertions.assertThat(title).isEqualTo("OrangeHRM");
+
+        
+    }
+
+    @DataProvider()
+    public Object[][] loginTestDataViaExcel() throws IOException {
+
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/testData/testdata.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet("Sheet1");
+
+        int row = sheet.getLastRowNum();
+        int col = sheet.getRow(0).getLastCellNum();
+
+        System.out.println("Row :" + row + "Column :" + col );
+
+
+        Object[][] data = new Object[row][col];
+
+
+        for (int i = 1; i <= row; i++) {
+            
+    
+                for (int j = 0; j < data.length; j++) {
+
+                    data[i-1][j] = sheet.getRow(i).getCell(j).getStringCellValue();
+                    
+                }
+                
+            
+        }
+
+        return data;
+        
         
     }
 
