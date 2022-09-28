0764fef12c8fea692bb4271f99c9fa97ee8d990b
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:12:45 -0400

    Framework Base class

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -6,20 +6,23 @@ import org.openqa.selenium.WebElement;
 import driver.DriverManager;
 import enums.WaitStrategy;
 import factories.ExplicitWaitFactory;
+import reports.ExtentLogger;
 
 public class BasePage {
 
-    protected void click(By by, WaitStrategy waitStrategy) {
+    protected void click(By by, WaitStrategy waitStrategy, String elementname) throws Exception {
 
         WebElement element = ExplicitWaitFactory.performingExplicitWait(waitStrategy, by);
         element.click();
+        ExtentLogger.pass(elementname+ " is clicked", true);
 
     }
 
-    protected void sendKeys(By by, String value, WaitStrategy waitStrategy) {
+    protected void sendKeys(By by, String value, WaitStrategy waitStrategy, String elementname) throws Exception {
 
         WebElement element = ExplicitWaitFactory.performingExplicitWait(waitStrategy, by);
         element.sendKeys(value);
+        ExtentLogger.pass(value+ " is entered sucessfully in "+ elementname, true);
 
     }
 
