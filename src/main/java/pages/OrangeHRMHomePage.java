2e9df9071dc05714dd9a166047cb7fcb3edde204
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:13:01 -0400

    Framework HomePage class

--- src/main/java/pages/OrangeHRMHomePage.java
+++ src/main/java/pages/OrangeHRMHomePage.java
@@ -9,16 +9,18 @@ public final class OrangeHRMHomePage extends BasePage {
     private final By linkWelcome = By.xpath("//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']");
     private final By linkLogout = By.xpath("//ul[@class='oxd-dropdown-menu']/li[4]/a");
 
-    public OrangeHRMHomePage clickWelcome() {
+    public OrangeHRMHomePage clickWelcome() throws Exception {
 
-        click(linkWelcome, WaitStrategy.CLICKABLE);
+        click(linkWelcome, WaitStrategy.CLICKABLE, "Welcome button");
+        // ExtentLogger.pass("Welcome Clicked");
         return this;
 
     }
 
-    public OrangeHRMLoginPage clickLogout() {
+    public OrangeHRMLoginPage clickLogout() throws Exception {
 
-        click(linkLogout, WaitStrategy.CLICKABLE);
+        click(linkLogout, WaitStrategy.CLICKABLE, "Logout Button");
+        // ExtentLogger.pass("Welcome Clicked");
         return new OrangeHRMLoginPage();
 
     }
