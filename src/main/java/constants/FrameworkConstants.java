97017ff846445a44cac0a057b1b532180e325573
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:11:44 -0400

    Framework Constants Declaration

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -1,5 +1,8 @@
 package constants;
 
+import enums.ConfigProperties;
+import utilities.PropertyUtils;
+
 public final class FrameworkConstants {
 
     private FrameworkConstants(){
@@ -11,18 +14,17 @@ public final class FrameworkConstants {
     private static final String FIREFOXDRIVERPATH = RESOURCESPATH + "/executables/geckodriver";
     private static final String CONFIGFILEPATH = RESOURCESPATH + "/config/config.properties"; 
     private static final int EXPLICITWAIT = 10; 
+    private static final String EXTENTREPORTPATH = System.getProperty("user.dir")+"/extent-test-output/";
+    private static String extentReportFilePath = "";
 
     public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
     }
-
-    
+ 
     public static String getFirefoxdriverpath() {
-        System.out.println(FIREFOXDRIVERPATH);
+        
         return FIREFOXDRIVERPATH;
     }
-
-
 
     public static String getConfigFilePath() {
         return CONFIGFILEPATH;
@@ -32,6 +34,27 @@ public final class FrameworkConstants {
         return EXPLICITWAIT;
     }
 
-    
+    public static String getExtentReportFilePath() throws Exception {
+        if (extentReportFilePath.isEmpty()) {
+            extentReportFilePath = createReportPath();
+            
+        }
+        return extentReportFilePath;
+    }
+
+    private static String createReportPath() throws Exception {
+
+        if (PropertyUtils.get(ConfigProperties.OVERRIDEREPORTS).equalsIgnoreCase("no")) {
+
+            return EXTENTREPORTPATH+"/"+System.currentTimeMillis()+"/index.html"; 
+        }
+        else{
+        return EXTENTREPORTPATH+"/index.html";
+        }
+    }
+
+
     
 }
+
+
