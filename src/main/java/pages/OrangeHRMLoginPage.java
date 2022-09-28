e0f97f0f3f691861c1a0cacc43a8ac040edd61b8
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:13:14 -0400

    Framework LoginPage class

--- src/main/java/pages/OrangeHRMLoginPage.java
+++ src/main/java/pages/OrangeHRMLoginPage.java
@@ -10,20 +10,20 @@ public final class OrangeHRMLoginPage extends BasePage{
     private final By textboxPassword = By.xpath("//input[@name = 'password' and @type='password']");
     private final By buttonLogin = By.xpath("//button[@type='submit']");
 
-    public OrangeHRMLoginPage enterUserName(String username) {
+    public OrangeHRMLoginPage enterUserName(String username) throws Exception {
 
-        sendKeys(textboxUsername, username, WaitStrategy.PRESENCE);
+        sendKeys(textboxUsername, username, WaitStrategy.PRESENCE, "Username");
         return this;
 
     }
 
-    public OrangeHRMLoginPage enterPassword(String password) {
-        sendKeys(textboxPassword, password, WaitStrategy.PRESENCE);
+    public OrangeHRMLoginPage enterPassword(String password) throws Exception {
+        sendKeys(textboxPassword, password, WaitStrategy.PRESENCE, "Password");
         return this;
     }
 
-    public OrangeHRMHomePage clickLogin() {
-        click(buttonLogin, WaitStrategy.CLICKABLE);
+    public OrangeHRMHomePage clickLogin() throws Exception {
+        click(buttonLogin, WaitStrategy.CLICKABLE, "Login Button");
         return new OrangeHRMHomePage();
 
     }
