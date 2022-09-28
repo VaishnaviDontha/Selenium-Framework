ea7c2c960b3d3a22d04d31237302b88de1a3adc8
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:14:03 -0400

    Framework Test file

--- src/test/java/tests/OrangeHRMTests.java
+++ src/test/java/tests/OrangeHRMTests.java
@@ -1,11 +1,5 @@
 package tests;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -19,84 +13,81 @@ public final class OrangeHRMTests extends BaseTest {
     }
 
     @Test(dataProvider = "loginTestData")
-    public void loginLogoutTests(String username, String password) {
+    public void loginLogoutTests(String username, String password) throws Exception {
+
+        // Reporting.createTest("loginlogouttests"); moving this to Base Test as this
+        // doesn't define any test data
 
         OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
-        
+
         String title = ohlp.enterUserName(username)
-                            .enterPassword(password)
-                            .clickLogin()
-                            .clickWelcome()
-                            .clickLogout()
-                            .getTitle();
+                .enterPassword(password)
+                .clickLogin()
+                .clickWelcome()
+                .clickLogout()
+                .getTitle();
 
         Assertions.assertThat(title).isEqualTo("OrangeHRM");
 
     }
-
 
     @DataProvider(parallel = true)
     public Object[][] loginTestData() {
 
-
-        return new Object[][]{
-            {"Admin","admin123"},
-            {"Admin12","admin"},
-            {"Admin","admin123"},
-            {"Admin","admin123"}
+        return new Object[][] {
+                { "Admin", "admin123" },
+                { "Admin", "admin12" },
+                { "Admin", "admin13" },
+                { "Admin", "admin23" }
 
         };
-        
+
     }
 
-    @Test(dataProvider = "loginTestDataViaExcel")
-    public void loginTestDataViaExcelTest(String uname, String pwd) {
+    // @Test(dataProvider = "loginTestDataViaExcel")
+    // public void loginTestDataViaExcelTest(String uname, String pwd) {
 
-        OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
-        
-        String title = ohlp.enterUserName(uname)
-                            .enterPassword(pwd)
-                            .clickLogin()
-                            .clickWelcome()
-                            .clickLogout()
-                            .getTitle();
+    // OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
 
-        Assertions.assertThat(title).isEqualTo("OrangeHRM");
+    // String title = ohlp.enterUserName(uname)
+    // .enterPassword(pwd)
+    // .clickLogin()
+    // .clickWelcome()
+    // .clickLogout()
+    // .getTitle();
 
-        
-    }
+    // Assertions.assertThat(title).isEqualTo("OrangeHRM");
 
-    @DataProvider()
-    public Object[][] loginTestDataViaExcel() throws IOException {
+    // }
 
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/testData/testdata.xlsx");
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-        XSSFSheet sheet = workbook.getSheet("Sheet1");
+    // @DataProvider()
+    // public Object[][] loginTestDataViaExcel() throws IOException {
 
-        int row = sheet.getLastRowNum();
-        int col = sheet.getRow(0).getLastCellNum();
+    // FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+
+    // "/src/main/java/testData/testdata.xlsx");
+    // XSSFWorkbook workbook = new XSSFWorkbook(fis);
+    // XSSFSheet sheet = workbook.getSheet("Sheet1");
 
-        System.out.println("Row :" + row + "Column :" + col );
+    // int row = sheet.getLastRowNum();
+    // int col = sheet.getRow(0).getLastCellNum();
 
+    // System.out.println("Row :" + row + "Column :" + col );
 
-        Object[][] data = new Object[row][col];
+    // Object[][] data = new Object[row][col];
 
+    // for (int i = 1; i <= row; i++) {
 
-        for (int i = 1; i <= row; i++) {
-            
-    
-                for (int j = 0; j < data.length; j++) {
+    // for (int j = 0; j < data.length; j++) {
 
-                    data[i-1][j] = sheet.getRow(i).getCell(j).getStringCellValue();
-                    
-                }
-                
-            
-        }
+    // data[i-1][j] = sheet.getRow(i).getCell(j).getStringCellValue();
 
-        return data;
-        
-        
-    }
+    // }
+
+    // }
+
+    // workbook.close();
+    // return data;
+
+    // }
 
 }
