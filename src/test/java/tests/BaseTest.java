cb4bb347c39d5cfa836dc1e24eec381178dade20
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-09-28 19:13:52 -0400

    Framework Base Test file

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,10 @@
 package tests;
 
+import java.lang.reflect.Method;
+
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
 
 import driver.Driver;
@@ -10,16 +14,23 @@ public class BaseTest {
     protected BaseTest() {
     }
 
-    @BeforeMethod
-    protected void setUp() throws Exception {
+    // @AfterSuite
+    // public void afterSuite() throws IOException {
+    // // Reporting.flushReports();
 
+    // }
+
+    @BeforeMethod
+    protected void setUp(Method m) throws Exception {
+
+        // Reporting.createTest(m.getName()); // retreiving method name via
+        // java.lang.reflect
         Driver.initDriver();
 
     }
 
     @AfterMethod
-    protected void tearDown() {
-
+    protected void tearDown(ITestResult result) {
         Driver.quitDriver();
 
     }
