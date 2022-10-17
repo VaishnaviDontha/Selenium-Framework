49cda0291f2a513a96b8c7d86a12800296fefbd0
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-10-17 10:47:52 -0400

    Cross Browser Testing, Reporting, etc add-ons

--- src/main/java/utilities/DataProviderUtils.java
+++ src/main/java/utilities/DataProviderUtils.java
@@ -4,34 +4,39 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import org.testng.annotations.DataProvider;
 
+import constants.FrameworkConstants;
+
 public final class DataProviderUtils {
 
-    private DataProviderUtils() {}
+    private DataProviderUtils() {
+    }
 
     @DataProvider
     public static Object[] getData(Method m) {
 
         String testname = m.getName();
+        System.out.println(testname);
 
-        List<Map<String,String>> list = ExcelUtils.getTestDetails("data");
+        List<Map<String, String>> list = ExcelUtils.getTestDetails("data");
 
-        List<Map<String,String>> refinedList = new ArrayList<>();
+        List<Map<String, String>> refinedList = new ArrayList<>();
 
-       for (int i = 0; i < list.size(); i++) {
-         if (list.get(i).get("testname").equalsIgnoreCase(testname)) {
-            if (list.get(i).get("execute").equalsIgnoreCase("yes")) {
-                refinedList.add(list.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).get("testname").equalsIgnoreCase(testname)) {
+                if (list.get(i).get("execute").equalsIgnoreCase("yes")) {
+                    refinedList.add(list.get(i));
+                }
+
             }
-             
-         }
-       }
+        }
 
+        System.out.println(refinedList);
         return refinedList.toArray();
-        
+
     }
 
-    
 }
