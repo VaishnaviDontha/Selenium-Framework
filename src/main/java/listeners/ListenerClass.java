99e7da60974de1e70e67f00175a0848379fd1176
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-12-10 17:57:39 -0500

    Updated Listener file

--- src/main/java/listeners/ListenerClass.java
+++ src/main/java/listeners/ListenerClass.java
@@ -11,27 +11,22 @@ import org.testng.ITestResult;
 import annotations.FrameworkAnnotation;
 import reports.ExtentLogger;
 import reports.Reporting;
+import utilities.ElkUtils;
 
 public class ListenerClass implements ITestListener, ISuiteListener {
 
     @Override
     public void onStart(ISuite suite) { // Before Suite calls
-        try {
+ 
             Reporting.initReports();
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
+       
     }
 
     @Override
     public void onFinish(ISuite suite) { // After Suite calls
-        try {
+   
             Reporting.flushReports();
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
+       
     }
 
     @Override
@@ -45,28 +40,23 @@ public class ListenerClass implements ITestListener, ISuiteListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         ExtentLogger.pass(result.getMethod().getMethodName() + " is Passed");
+        ElkUtils.sendDetailsToElk(result.getMethod().getMethodName(), "pass");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        try {
+        
             ExtentLogger.fail(result.getMethod().getMethodName() + "is Failed", true);
             ExtentLogger.fail(result.getThrowable().toString()); // to grab top-most log from the exception
             ExtentLogger.fail(Arrays.toString(result.getThrowable().getStackTrace())); // to grab complete failed o/p
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
+            ElkUtils.sendDetailsToElk(result.getMethod().getMethodName(), "fail");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        try {
-            ExtentLogger.skip(result.getMethod().getMethodName() + "is Skipped", true);
-        } catch (Exception e) {
         
-            e.printStackTrace();
-        }
+            ExtentLogger.skip(result.getMethod().getMethodName() + "is Skipped", true);
+            ElkUtils.sendDetailsToElk(result.getMethod().getMethodName(), "skip");
     }
 
     @Override
