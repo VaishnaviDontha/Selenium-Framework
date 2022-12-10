304c377a6c983e97bd974faaab3dd51e29782636
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-12-10 17:54:39 -0500

    Updated Driver file

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -4,10 +4,22 @@ import java.util.Objects;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.Browser;
 import constants.FrameworkConstants;
 import enums.ConfigProperties;
+import exceptions.BrowserInvocationFailedException;
+import factories.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utilities.PropertyUtils;
+
+/**
+ * 
+ * Driver class - Initiating/Closing Browser
+ * 
+ * @author Vaishnavi
+ * @version 1.0
+ */
 
 public final class Driver {
 
@@ -15,20 +27,26 @@ public final class Driver {
 
     }
 
-    public static void initDriver(String browser) throws Exception {
+    /**
+     * The initDriver function is used to initialize the driver.
+     * 
+     *
+     * @param browser Determine which browser to use
+     *
+     * @return The driver object
+     *
+     * @docauthor Trelent
+     */
+    public static void initDriver(String browser) {
+
+        String runmode = PropertyUtils.get(ConfigProperties.RUNMODE);
 
         if (Objects.isNull(DriverManager.getDriver())) {
-
-            if (browser.equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver", FrameworkConstants.getChromeDriverPath());
-                DriverManager.setDriver(new ChromeDriver());
+            try {
+                DriverManager.setDriver(DriverFactory.getDriver(browser));
+            } catch (Exception e) {
+                throw new BrowserInvocationFailedException("Please check the capabilities");
             }
-
-            else if (browser.equalsIgnoreCase("firefox")) {
-                System.setProperty("webdriver.gecko.driver", FrameworkConstants.getFirefoxdriverpath());
-                DriverManager.setDriver(new FirefoxDriver());
-            }
-
             DriverManager.getDriver().get(PropertyUtils.get(ConfigProperties.URL));
         }
 
